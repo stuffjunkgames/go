8140f02b9e60e1ee52d1d3b8e10da95a38c184fc
stuffjunkgames <msquared.169@gmail.com>
2016-03-16 20:07:27 -0400

    update laptop

--- main.cpp
+++ main.cpp
@@ -6,28 +6,55 @@
 
 #define GAME_TOP 0
 #define GAME_LEFT 0
-#define GAME_WIDTH 1024
-#define GAME_HEIGHT 768
+#define GAME_WIDTH 19
+#define GAME_HEIGHT 19
+const int TILE_SIZE = 50;
 #define GAME_RIGHT GAME_LEFT+GAME_WIDTH
 #define GAME_BOTTOM GAME_TOP+GAME_HEIGHT
 
-public class BoardSection
-{
-public:
-    static const std::string cornerIntersectionFile = "corner.tif";
-    static const std::string edgeIntersectionFile = "edge.tif";
-    static const std::string intersectionFile = "intersection.tif";
-    static const std::string stoneFile = "stone.tif";
-}
+
+static const std::string cornerIntersectionFile = "corner.tif";
+static const std::string edgeIntersectionFile = "edge.tif";
+static const std::string intersectionFile = "intersection.tif";
+static const std::string stoneFile = "stone.tif";
+
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1024, 768), "Go");
+    sf::RenderWindow window(sf::VideoMode(GAME_WIDTH * TILE_SIZE, GAME_HEIGHT * TILE_SIZE), "Go");
 
     sf::Time dt;
     sf::Time t;
     sf::Clock clock;
 
+    sf::Image cornerImg;
+    sf::Image edgeImg;
+    sf::Image intersectionImg;
+    sf::Image stoneImg;
+
+    if(!cornerImg.loadFromFile(cornerIntersectionFile))
+    {
+        std::cout << "Failed to load corner image file\n";
+        return 1;
+    }
+
+    if(!edgeImg.loadFromFile(edgeIntersectionFile))
+    {
+        std::cout << "Failed to load edge image file\n";
+        return 1;
+    }
+    if(!intersectionImg.loadFromFile(intersectionFile))
+    {
+        std::cout << "Failed to load intersection image file\n";
+        return 1;
+    }
+    if(!stoneImg.loadFromFile(stoneFile))
+    {
+        std::cout << "Failed to load stone image file\n";
+        return 1;
+    }
+
+    sf::Sprite background[GAME_WIDTH][GAME_HEIGHT];
 
     while (window.isOpen())
     {
